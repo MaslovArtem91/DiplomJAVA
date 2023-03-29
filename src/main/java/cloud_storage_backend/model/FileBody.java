71821ef67e360a752555d2f4bef5d35d3455efda
Maslov Artem <amaslov91@mail.ru>
2023-03-29 16:57:42 +0300

    first commit

---
+++ src/main/java/cloud_storage_backend/model/FileBody.java
@@ -0,0 +1,13 @@
+package cloud_storage_backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileBody {
+
+    private String fileName;
+}
---
+++ src/main/java/cloud_storage_backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package cloud_storage_backend.repository;
+
+import cloud_storage_backend.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findUserByLogin(String login);
+}
